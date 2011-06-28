db4099868c21b0ea290da16aef2ca5f1c89dbbf5
cairey <cairey01@hotmail.com>
2011-06-28 16:47:08 +0100

    Simple.Data going in

--- source/LetMeRate.Application/AccountService.cs
+++ source/LetMeRate.Application/AccountService.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Simple.Data;
 
 namespace LetMeRate.Application
 {
     public class AccountService : IAccountService
     {
+
+
         public void CreateAccount(string email, string password)
         {
-            throw new NotImplementedException();
+            var db = Database.Open();
+            var customer = db.UserAccount.Insert(Email: email, Password: password);
         }
     }
 }
