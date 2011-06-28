892d3eda1ec43d0ade17229816b61956b2249755
cairey <cairey01@hotmail.com>
2011-06-28 09:48:12 +0100

    project setup

---
+++ source/LetMeRate.Web/MainModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace LetMeRate.Web
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+           
+            Get["/"] = x =>
+            {
+                return "Test Route";
+            };
+
+            Get["/test"] = x =>
+            {
+                return "Test";
+            };
+        }
+    }
+}
