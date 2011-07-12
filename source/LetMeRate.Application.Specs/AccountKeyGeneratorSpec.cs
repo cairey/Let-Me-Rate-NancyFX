18dade02a814df2c4ec5e7cf34cfc37dff3716d8
cairey <cairey01@hotmail.com>
2011-07-12 21:01:36 +0100

    added mspec

---
+++ source/LetMeRate.Application.Specs/AccountKeyGeneratorSpec.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LetMeRate.Application.Security;
+using Machine.Specifications;
+
+namespace LetMeRate.Application.Specs
+{
+    [Subject("When generating an account key")]
+    public class AccountKeyGeneratorSpec
+    {
+        private Establish context = () =>
+                                        {
+                                            accountKeyGenerator = new AccountKeyGenerator();
+                                        };
+
+
+        private Because of = () =>
+                                 {
+                                     key = accountKeyGenerator.CreateKey();
+                                     key2 = accountKeyGenerator.CreateKey();
+                                 };
+
+        private It should_have_created_a_key = () =>
+                                                   {
+                                                       key.ShouldNotBeEmpty();
+                                                       key.ShouldNotBeNull();
+                                                       key.ShouldNotEqual(key2);
+                                                   };
+
+
+        private static AccountKeyGenerator accountKeyGenerator;
+        private static string key;
+        private static string key2;
+    }
+}
---
+++ source/LetMeRate.Application.Specs/SecurityDigestSpec.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LetMeRate.Application.Security;
+using Machine.Specifications;
+
+namespace LetMeRate.Application.Specs
+{
+    [Subject("When I want to be authenticated")]
+    public class when_I_want_to_be_authenticated
+    {
+        private Establish context = () =>
+        {
+            _securityDigest = new SecurityDigest();
+            _plainTextPhase = "pa55word";
+            _salt = _securityDigest.CreateSalt();
+            _hashedPhase = _securityDigest.EncryptPhase(_plainTextPhase, _salt);
+        };
+
+        private Because of = () =>
+                                 {
+                                     _authenticated = _securityDigest.Authenticated("pa55word", _hashedPhase, _salt);
+                                 };
+
+        private It should_be_a_match = () => _authenticated.ShouldBeTrue();
+
+
+
+        private static string _plainTextPhase;
+        private static SecurityDigest _securityDigest;
+        private static string _salt;
+        private static string _hashedPhase;
+        private static bool _authenticated;
+    }
+
+
+    [Subject("When I don't want to be authenticated")]
+    public class when_I_dont_want_to_be_authenticated
+    {
+        private Establish context = () =>
+        {
+            _securityDigest = new SecurityDigest();
+            _plainTextPhase = "pa55word";
+            _salt = _securityDigest.CreateSalt();
+            _hashedPhase = _securityDigest.EncryptPhase(_plainTextPhase, _salt);
+        };
+
+        private Because of = () =>
+        {
+            _authenticated = _securityDigest.Authenticated("password", _hashedPhase, _salt);
+        };
+
+        private It should_be_a_match = () => _authenticated.ShouldBeFalse();
+
+
+        private static string _plainTextPhase;
+        private static SecurityDigest _securityDigest;
+        private static string _salt;
+        private static string _hashedPhase;
+        private static bool _authenticated;
+    }
+}
